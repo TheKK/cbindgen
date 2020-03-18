a21ad93a3ad8d87e9a45a0b488283c730ae654cc
TheKK <thumbd03803@gmail.com>
2020-03-18 13:31:16 +0800

    tests: modify raw_identifier.rs to make test failed

--- tests/expectations/raw_identifier.compat.c
+++ tests/expectations/raw_identifier.compat.c
@@ -3,13 +3,48 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define r#CONST_RAW_ID 42
+
+typedef enum {
+  r#Bar,
+} r#Foo;
+
+typedef struct Node_r#Foo Node_r#Foo;
+
+typedef struct {
+  r#Foo r#raw_ident_field_x;
+} r#RawIdentStruct;
+
+typedef r#Foo r#OtherFoo;
+
+typedef union {
+  r#Foo r#raw_ident_union_field_a;
+  r#Foo r#raw_ident_union_field_b;
+} r#RawIdentUnion;
+
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
 
+extern const r#Foo r#GLOBAL_RAW_IDENT;
+
+extern r#Foo r#MUT_GLOBAL_RAW_IDENT;
+
 extern void async(void);
 
-void match(void);
+void enum_root(Foo foo);
+
+void generic_path_root(const Node_r#Foo *node);
+
+void raw_ident_function(void);
+
+void raw_ident_function_argument(int32_t r#param);
+
+void struct_root(const r#RawIdentStruct *s);
+
+void typedef_root(r#OtherFoo t);
+
+void union_root(r#RawIdentUnion u);
 
 #ifdef __cplusplus
 } // extern "C"
--- tests/rust/raw_identifier.rs
+++ tests/rust/raw_identifier.rs
@@ -1,6 +1,64 @@
+// Global
 #[no_mangle]
-pub extern "C" fn r#match() {}
+pub static mut r#MUT_GLOBAL_RAW_IDENT: r#Foo = r#Foo::r#Bar;
 
+#[no_mangle]
+pub static r#GLOBAL_RAW_IDENT: r#Foo = r#Foo::r#Bar;
+
+// Constant
+pub const r#CONST_RAW_ID: i32 = 42;
+pub const r#CONST_FOO: r#FOO = r#FOO::r#Bar;
+
+// Structure
+#[repr(C)]
+pub struct r#RawIdentStruct {
+	r#raw_ident_field_x: r#Foo,
+}
+
+#[no_mangle]
+pub extern "C" fn r#struct_root(s: &r#RawIdentStruct) {}
+
+// Enumeration
+#[repr(C)]
+pub enum r#Foo {
+	r#Bar,
+}
+
+#[no_mangle]
+pub extern "C" fn r#enum_root(foo: r#Foo) {}
+
+// Function
+#[no_mangle]
+pub extern "C" fn r#raw_ident_function() {}
+
+#[no_mangle]
+pub extern "C" fn r#raw_ident_function_argument(r#param: i32) {}
+
+// Extern function
 extern "C" {
     fn r#async();
 }
+
+// Generic path
+struct Node<T> {
+	t: t,
+}
+
+#[no_mangle]
+pub extern "C" fn r#generic_path_root(node: &Node<r#Foo>) {}
+
+// Union
+#[repr(C)]
+union r#RawIdentUnion {
+    r#raw_ident_union_field_a: r#Foo,
+    r#raw_ident_union_field_b: r#Foo,
+}
+
+#[no_mangle]
+pub extern "C" fn r#union_root(u: r#RawIdentUnion) {}
+
+// Typedef
+pub type r#OtherFoo = r#Foo;
+
+#[no_mangle]
+pub extern "C" fn r#typedef_root(t: r#OtherFoo) {}
